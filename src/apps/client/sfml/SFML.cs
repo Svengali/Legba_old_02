1fbdf11ab3490aba6a4e0abc46bf27ba4296504b
Marc Hernandez <marcsh@gmail.com>
2021-01-01 15:00:55 -0800

    x) Add SFML as a library x) Add a SFML client.

---
+++ src/apps/client/sfml/SFML.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+
+using lib;
+
+using Optional;
+
+namespace cl
+{
+
+
+
+
+
+	class Client
+	{
+
+		private static bool s_running = true;
+
+		private static void onAppExit( object sender, EventArgs e )
+		{
+			s_running = false;
+		}
+
+
+
+		static void Main( string[] args )
+		{
+
+			sv.Main server = new sv.Main( args[0] );
+
+			server.startup();
+
+			while( s_running )
+			{
+				System.Threading.Thread.Sleep( 1 );
+			}
+
+			server.shutdown();
+
+
+
+
+		}
+
+		#region Test
+
+
+		#endregion //Test
+
+	}
+}
